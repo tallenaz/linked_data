9e84a7d4a125b1fc56bddfd4b389183b143d8835
Anthony Zanella <azanella@stanford.edu>
2016-06-14 14:23:44 -0700

    Add ability to list albums

--- lib/PerformanceGroup.rb
+++ lib/PerformanceGroup.rb
@@ -1,3 +1,9 @@
 class PerformanceGroup
-  attr_accessor :name
+  attr_accessor :name, :albums
+
+  def list_albums
+    output = ''
+    @albums.each { |a| output += a + "\n" }
+    output
+  end
 end
--- spec/lib/performance_group_spec.rb
+++ spec/lib/performance_group_spec.rb
@@ -2,13 +2,19 @@ require 'spec_helper'
 require 'PerformanceGroup'
 
 describe 'a PerformanceGroup' do
+  before :each do
+    @foo = PerformanceGroup.new
+  end
   it 'can instantiate' do
-    foo = PerformanceGroup.new
-    expect(foo).to be_kind_of PerformanceGroup
+    expect(@foo).to be_kind_of PerformanceGroup
   end
   it 'has a name' do
-    foo = PerformanceGroup.new
-    foo.name = 'John Lennon'
-    expect(foo.name).to eq 'John Lennon'
+    @foo.name = 'John Lennon'
+    expect(@foo.name).to eq 'John Lennon'
+  end
+  it 'has a list of albums' do
+    albums = ['Hard Days Night', 'White Album', "I don't know"]
+    @foo.albums = albums
+    expect(@foo.list_albums).to eq "Hard Days Night\nWhite Album\nI don't know\n"
   end
 end
